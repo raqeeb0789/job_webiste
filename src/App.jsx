da0a3542a9cc383d5d45377e86b9bcaa83480314
raqeeb0789 <abdulskraqeeb@gmail.com>
2024-12-30 20:08:42 +0530

    day-1 completing the intial web page

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import Hero from "./Hero";
+import Footer from "./Footer";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Hero />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
